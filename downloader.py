31f7f5bdc3bc409467bd48e543547d376ac33ae5
klondike <noelejoshua@gmail.com>
2020-06-24 01:50:46 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -15,7 +15,7 @@ global location
 
 location = os.getcwd() + "\\Downloads" #Location where downloaded videos go
 
-def youtubeDL(): #Downloder function
+def youtubeDL(): #Downloader function
     subprocess.run(["cls"], shell= True)
     url = input("Please enter the youtube video url:\n")
     fileType = ""
